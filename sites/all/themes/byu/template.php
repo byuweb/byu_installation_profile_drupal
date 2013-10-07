05d23b6280d776ee384f6da886fd1920067e740b
Sherwin Harris <sherwin@byu.edu>
2013-10-07 10:34:53 -0600

    fixing css for featured items

--- sites/all/themes/byu/template.php
+++ sites/all/themes/byu/template.php
@@ -11,8 +11,8 @@
  function byu_preprocess_html(&$variables) {
   drupal_add_css('http://cloud.typography.com/75214/740862/css/fonts.css', array('type' => 'external'));
 }
- 
- 
+
+
 /**
  * Creates the variables to be used in the template (page.tpl.php is a view and should not have logic.)
  * @param array $variables
@@ -29,7 +29,7 @@ function  byu_form_alter(&$form, &$form_state, $form_id) {
 
 /**
  * Implements theme_menu_item()
- * 
+ *
  * Used to modify the markup of the menus. This is to accommodate for when the megamenu module isn't being used.
  */
 
@@ -39,7 +39,7 @@ function byu_menu_link(array $variables) {
 	$third_level = '';
 
 	$output = l($element['#title'], $element['#href'], $element['#localized_options']);
-	
+
 	if ($element['#below'] && $element['#original_link']['depth'] == 1) {
 		$sub_menu = drupal_render($element['#below']);
 	} elseif ($element['#below'] && $element['#original_link']['depth'] == 2) {
@@ -55,8 +55,8 @@ function byu_menu_link(array $variables) {
 	return $html;
 }
 
- 
-/** 
+
+/**
  * Return a themed breadcrumb trail.
  *
  * @param $variables
@@ -72,15 +72,15 @@ function byu_menu_link(array $variables) {
 function byu_breadcrumb($variables) {
 	global $base_path;
 	global $theme_path;
-	$breadcrumb = $variables['breadcrumb']; //This is a variable for your page.tpl.php 
+	$breadcrumb = $variables['breadcrumb']; //This is a variable for your page.tpl.php
 	$breadcrumb_separator = ' › ';
 	if(!drupal_is_front_page()){
-	
+
 		// Replace "Home" with the name of the website
 		$home = array_shift($breadcrumb);
 		$site_name = variable_get('site_name');
 		array_unshift($breadcrumb,str_replace("Home",$site_name,$home));
-    
+
 
 		// Style breadcrumb so that only the last two appear on the page; the rest will be shown in a div like on lds.org
 		$finalBreadcrumb = array_slice($breadcrumb,-1,1);
@@ -89,14 +89,14 @@ function byu_breadcrumb($variables) {
 		if(empty($finalBreadcrumb[0])) { // Sometimes, the home page isn't loaded into the breadcrumb. this is my fix for that.
 			$finalBreadcrumb[0] = '<a href="' . $base_path . '">' . $site_name . '</a>';
 		}
-	  
+
 		// Build the breadcrumb trail.
 		$output = '<ul class="breadcrumb">';
 		$output .= '<li><div class="dropdown"><a href="" data-toggle="dropdown" class="home dropdown-toggle needsclick"><i class="icon-home icon-large"></i>';
 		$output .= '<span class="visuallyhidden">Home</span><i class="icon-caret-down"></i></a>';
 		$output .= '<ul class="dropdown-menu"><li>' . implode("" . '</li><li>', $breadcrumb) . '</li></ul>';
 		$output .= '<i class="divider icon-angle-right"></i></div></li>';
-		$output .= '<li>' . implode($breadcrumb_separator . '</li><li>', $finalBreadcrumb) . '<i class="divider icon-angle-right"></i></li>';      
+		$output .= '<li>' . implode($breadcrumb_separator . '</li><li>', $finalBreadcrumb) . '<i class="divider icon-angle-right"></i></li>';
 		$output .= '<li class="active">' . drupal_get_title() . '</li>';
 		$output .= '</ul>';
 		return $output;
